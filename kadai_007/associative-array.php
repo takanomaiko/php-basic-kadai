ec94a1315f8e278231b3fd1633a2699c1a98422d
takanomaiko <takanomaiko0816@gmail.com>
2024-10-23 16:07:54 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        $commodity = ['name' => 'onion' , 'price' => 200, 'weight' => 160];
+        print_r($commodity);
+        ?>
+    </p>
+</body>
+</html>
