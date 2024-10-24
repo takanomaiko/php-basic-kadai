92a48827184197047e93391c1d2dd89ec85fc097
takanomaiko <takanomaiko0816@gmail.com>
2024-10-24 11:31:46 +0900

    kadai_011をアップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題011</title>
+</head>
+<body>
+    <p>
+        <?php
+        $commodity = ['名前' => '玉ねぎ' , '値段' => 200, '産地' => '北海道'];
+        foreach ($commodity as $key => $value){
+            echo "{$key}：{$value}<br>";
+        }
+        ?>
+    </p>
+</body>
+</html>
